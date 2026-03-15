8a09c705e3045444df05757acffddf50d7a88ed3
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-15 20:04:40 +0100

    Added method to split data.

--- src/KNN.java
+++ src/KNN.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class KNN {
@@ -27,6 +29,37 @@ public class KNN {
             }
             data.add(new Iris(features, parts[4]));
         }
+    }
 
+    static void splitData(double trainRatio){
+        List<Iris> irisSetosa = new ArrayList<>();
+        List<Iris> irisVersicolor = new ArrayList<>();
+        List<Iris> irisVirginica = new ArrayList<>();
+
+        for (Iris datum : data) {
+            if (datum.label.equals("Iris-setosa"))
+                irisSetosa.add(datum);
+            else if (datum.label.equals("Iris-versicolor"))
+                irisVersicolor.add(datum);
+            else
+                irisVirginica.add(datum);
+        }
+
+        Collections.shuffle(irisSetosa);
+        Collections.shuffle(irisVersicolor);
+        Collections.shuffle(irisVirginica);
+
+        int trainSize = (int)(data.size() * trainRatio / 3);
+
+        for (int i = 0; i < trainSize; i++) {
+            train.add(irisSetosa.get(i));
+            train.add(irisVersicolor.get(i));
+            train.add(irisVirginica.get(i));
+        }
+        for (int i = trainSize; i < data.size() - 1; i++) {
+            test.add(irisSetosa.get(i));
+            test.add(irisVersicolor.get(i));
+            test.add(irisVirginica.get(i));
+        }
     }
 }
