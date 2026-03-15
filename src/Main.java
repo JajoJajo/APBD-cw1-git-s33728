fb8cab3bce193cc6bae5b78b8896372d1b55145b
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-15 21:49:33 +0100

    Edited main to better test all features

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main() {
-        try {
-            KNN.loadData("data/iris.txt");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        KNN.splitData(0.5);
-        for (Iris data : KNN.test)
-            System.out.println(data);
-        System.out.println(KNN.test.size());
-        System.out.println(KNN.train.size());
+    public static void main() throws Exception {
+        KNN.loadData("data/iris.txt");
+
+        KNN.splitData(0.75);
 
         KNN.evaluateModel();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("\n1 - Klasyfikuj ręcznie");
+            System.out.println("2 - Test z pliku");
+            System.out.println("0 - Wyjście");
+
+            int choice = sc.nextInt();
+
+            if (choice == 1) KNN.manualInput(sc);
+            else if (choice == 2) KNN.fileTest(sc);
+            else break;
+        }
     }
 }
