f64b66b2da020e1ee01ee95fb3a5c34a266771fb
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-15 19:34:13 +0100

    Initial project

---
+++ src/Iris.java
@@ -0,0 +1,9 @@
+public class Iris {
+    double[] features;
+    String label;
+
+    public Iris(double[] features, String label) {
+        this.features = features;
+        this.label = label;
+    }
+}
